ebf703691e23b19928b10dd1f56cf90fc177e90f
janvi-31 <janvi.kadam@mldc.edu.in>
2025-10-06 18:58:54 +0530

    removed extra cors

--- backend/index.js
+++ backend/index.js
@@ -28,19 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-        'Access-Control-Allow-Origin',
-        'https://desi-etsy-delta.vercel.app'
-    );
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-});
 
 db();
 
